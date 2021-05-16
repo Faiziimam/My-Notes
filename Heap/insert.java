af47f3157f92ea30d27ec21a97d89d9d1c7316b2
Faiz Imam <70627962+Faiziimam@users.noreply.github.com>
2021-05-16 20:37:26 +0530

    made the code clean

--- Heap/insert.java
+++ Heap/insert.java
@@ -1,24 +1,30 @@
-public class Heap {
+package com.company;
+
+public class Main {
     private int[] item=new int[10];
     private int size;
 
     public void insert(int data)
     {
-        if(size==item.length)
+        if(isFull())
         {
             throw new IllegalStateException();
         }
         item[size++]=data;
-        var index=size-1;
-        var parentIndex=(index-1)/2;
-
-        while(index>0 && item[index]>item[parentIndex])
-        {
-            swap(index,parentIndex);
-            index=(index-1)/2;
+        
+        //This function swap the nodes if the child node is larger than the parent node.
+        bubbleUp();
+    }
+    public void bubbleUp() {
+        var index = size - 1;
+        var parentIndex = (index - 1) / 2;
+        while (index > 0 && item[index] > item[parentIndex]) {
+            swap(index, parentIndex);
+            index = (index - 1) / 2;
         }
     }
-
+    //This will check if the size has become equal to the size of the array.
+  public boolean isFull() { return (size==item.length); }
     private void swap(int first, int second) {
         var temp=item[first];
         item[first]=item[second];
@@ -28,7 +34,7 @@ public class Heap {
 
     public static void main(String[] args)
     {
-         Heap heap=new Main();
+         Main heap=new Main();
          heap.insert(10);
          heap.insert(5);
          heap.insert(17);
