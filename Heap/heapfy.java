a1371ac5fa2c6ad9dfab2e3e1fbfdbdb3c9caf24
Faiz Imam <70627962+Faiziimam@users.noreply.github.com>
2021-05-19 21:23:34 +0530

    Create heapfy.java

---
+++ Heap/heapfy.java
@@ -0,0 +1,31 @@
+ public void heapify(int[] arr)
+  {
+     for(int i=0;i<arr.length;i++)
+     {
+         heapify(arr,i);
+     }
+  }
+  public void heapify(int[] arr,int index) {
+      var largestIndex = index;  //Assuming index is our largestElement(root)
+      var leftIndex = index * 2 + 1;
+      var rightIndex = index * 2 + 2;
+
+          if (leftIndex < arr.length && arr[leftIndex] > arr[index]) {
+              largestIndex = leftIndex;
+          }
+          if (rightIndex < arr.length && arr[rightIndex] > arr[index]) {
+              largestIndex = rightIndex;
+          }
+          if (index == largestIndex) {
+              return;
+          }
+          swap(arr,index, largestIndex);
+          heapify(arr,largestIndex);
+
+  }
+    public void swap(int[] arr,int first,int sec)
+    {
+        int temp=arr[first];
+        arr[first]=arr[sec];
+        arr[sec]=temp;
+    }
