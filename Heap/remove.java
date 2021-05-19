41d931942005b0e54c7c599bbaeb020405f022de
Faiz Imam <70627962+Faiziimam@users.noreply.github.com>
2021-05-19 12:36:19 +0530

    Refactor

--- Heap/remove.java
+++ Heap/remove.java
@@ -1,11 +1,13 @@
- public void remove()
-    {
+ public int remove()
+    {   
         if(isEmpty())
         {
             throw new IllegalStateException();
         }
+        var root=item[0];
         item[0]=item[--size];
         bubbleUp();
+      return root;
     }
  //This function will work if the root node data is smaller than the child node data (it will swap the data of the larger child to the root node) 
     public void bubbleDown()
@@ -19,8 +21,25 @@
         }
     }
 // To distinguish whilch child node have larger data;
-    public int largerChildIndex(int index) { return (leftIndex(index)> rightIndex(index))? leftChildIndex(index) : rightChildIndex(index); }
+    public int largerChildIndex(int index) {
+     
+     //      Sidecases
+        if(!hasLeftChild(index))
+        {
+            return index;
+        }
+        if(!hasRightChild(index))
+        {
+            return leftChildIndex(index);
+        }
+     
+     return (leftIndex(index)> rightIndex(index))? leftChildIndex(index) : rightChildIndex(index); }
   
+
+//     To handle Side Cases
+    public boolean hasLeftChild(int index){ return leftChildIndex(index) <= size; }
+    public boolean hasRightChild(int index){ return rightChildIndex(index) <= size; }
+
 // These two functions are use to find the index of left and right child respectivly
     public int leftIndex(int index) { return item[leftChildIndex(index)]; }
     public int rightIndex(int index) { return item[rightChildIndex(index)]; }
@@ -31,5 +50,15 @@
 // This will tell you wether you need to perform bubble downn operation or not
     public boolean isValid(int index)
     {
-        return item[index]>=leftIndex(index) && item[index]>=rightIndex(index);
+//           SideCases
+        if(!hasLeftChild(index))
+        {
+            return true;
+        }
+        var isValid=item[index] >=item[leftChildIndex(index)];
+        if(hasRightChild(index))
+        {
+            isValid &=item[index] >= item[rightChildIndex(index)];
+        }
+        return isValid;
     }
