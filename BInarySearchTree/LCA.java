30044827dfff092d44e64c15b6dfb2a8e6f719ce
Faiz Imam <70627962+Faiziimam@users.noreply.github.com>
2021-06-03 20:32:47 +0530

    Create LCA.java

---
+++ BInarySearchTree/LCA.java
@@ -0,0 +1,29 @@
+// LCA is basically a common ancestor between both the nodes.
+//  for example  :   
+//               1
+//             2   3
+//           4  5    6
+//     Q we have to find LCA between [4,6]
+//     Ans: we will traverse from the node for 4 we have [1,2,3]
+//                                         for 6 we have [1,3,6]
+//          In both of the above traversal we have 1 in common so LCA is 1
+         
+//   logic
+     
+     public node LCA(node root,int n1,n2)
+     {
+       if(root==null)
+       {
+        return null;
+       }
+       if(root.data==n1 || root.data==n2)
+          return node;
+         
+       node left=LCA(root.left,n1,n2);
+       node rightLCA(root.right,n1,n2);
+       
+       if(left!=null && right!=null)
+          return node;
+       
+       return (left!=null) ? left: right;
+     }
