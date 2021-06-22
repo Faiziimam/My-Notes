208f9d909d73916fdf172fbc38a1e071b7411b2e
Faiz Imam <70627962+Faiziimam@users.noreply.github.com>
2021-06-22 22:13:05 +0530

    Create Cummulative-Sum.java

---
+++ Array/subarray/Cummulative-Sum.java
@@ -0,0 +1,20 @@
+public void cummulative(int[] a)
+{
+  int maxSum=Integer.MIN_VALUE;
+  int currentSum[]=new int[a.length+1];
+  currentSum[0]=0;
+  for(int i=1;i<a.length;i++)
+  {
+      currentSum[i]=currentSum[i-1]+a[i-1];
+  }
+  for(int i=1;i<a.length;i++)
+  {
+   maxSum=Math.max(maxSum,currentSum[i]);
+    for(int i=1;i<a.length;i++)
+    {
+        sum=currentSum[i]+currentSum[j-1];
+         maxSum=Math.max(maxSum,sum);
+    }
+  }
+    System.out.print(maxSum);
+}
