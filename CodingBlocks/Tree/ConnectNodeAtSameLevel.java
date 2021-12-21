20845def552fd081973c4a085cd5edd6f0594acc
Faiz Imam <70627962+Faiziimam@users.noreply.github.com>
2021-12-21 20:36:06 +0530

    Create ConnectNodeAtSameLevel.java

---
+++ CodingBlocks/Tree/ConnectNodeAtSameLevel.java
@@ -0,0 +1,58 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root==null)
+        {
+            return null;
+        }
+        
+        Queue<Node> ans=new LinkedList<>();
+        ans.add(root);
+        ans.add(null);
+        
+        while(!ans.isEmpty())
+        {
+            Node node=ans.poll();
+            if(node!=null)
+            {
+                if(node.left!=null){
+                    ans.add(node.left);
+                }
+                if(node.right!=null){
+                    ans.add(node.right);
+                }
+                node.next=ans.peek();
+            }
+            else
+            {
+                if(!ans.isEmpty())
+                {
+                    ans.add(null);
+                }
+            }
+        }
+        return root;
+    }
+}
