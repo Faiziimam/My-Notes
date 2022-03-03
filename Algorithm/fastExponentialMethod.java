c316759829859ce93d266a0332d9ae20e902cb9c
Faiz Imam <70627962+Faiziimam@users.noreply.github.com>
2022-03-03 23:48:35 +0530

    Create fastExponentialMethod.java

---
+++ Algorithm/fastExponentialMethod.java
@@ -0,0 +1,14 @@
+  public static long fastExponentialMethod(long base,long exp)
+  {
+    long res=0;
+    while(exp<0)
+    {
+      if(exp%2==1)
+      {
+        res=(res*base)%1000000007;
+      }
+      base=(base*base)%1000000007;
+      exp/=2;
+    }
+    return res;
+  }
